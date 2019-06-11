fb8165b95327578c91d71223005b7421dbbe22a2
Jose Manuel Bonoko Bijeri <jbonoko@alpha30.aulas.gsyc.urjc.es>
2019-06-11 18:11:34 +0200

    PRACTICA 4

--- client.py
+++ client.py
@@ -4,19 +4,29 @@
 Programa cliente UDP que abre un socket a un servidor
 """
 
+import sys
 import socket
 
 # Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = 'localhost'
-PORT = 6001
-LINE = '¡Hola mundo!'
+try:
+    SERVER = str(sys.argv[1])
+    PORT = int(sys.argv[2])
+    METOD = sys.argv[3]
+    ADRESS = sys.argv[4]
+    TIME = sys.argv[5]
+
+except (IndexError, ValueError):
+    print("Usage: client.py ip puerto register sip_address expires_value")
+
+if METOD == 'register':
+    LINE = ('sip:' + ADRESS + "SIP/2.0\r\n\r\n" + 'expire_time:' + TIME + '\r\n\r\n')
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
+    print("Enviando:", LINE, end='')
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
+    print('Recibido -- ', data.decode('utf-8'), end='')
 
 print("Socket terminado.")
--- server.py
+++ server.py
@@ -3,31 +3,72 @@
 """
 Clase (y programa principal) para un servidor de eco en UDP simple
 """
-
+import sys
+import json
+import time
 import socketserver
 
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
+    """Echo server class."""
 
-class EchoHandler(socketserver.DatagramRequestHandler):
-    """
-    Echo server class
-    """
+    dicc_registers = {}
 
-    def handle(self):
-        """
-        handle method of the server class
-        (all requests will be handled by this method)
-        """
-        self.wfile.write(b"Hemos recibido tu peticion")
+    def register2json(self):
+        """Convertir a json."""
+        json.dump(self.dicc_registers, open('registed.json', 'w'), indent=3)
+
+    def json2registered(self):
+        """Si existe file_json lo pasa a mi diccionario."""
+        try:
+            with open('registed.json', 'r') as file_json:
+                self.dicc_registers = json.load(file_json)
+        except FileNotFoundError:
+            pass
+
+    """ Comprobar si un usuario está caducado """
+    def expires(self):
+        i = 1
+        while n <= len(self.diclient):
+            for user in self.diclient:
+                now = time.ctime(time.time())
+                if self.diclient[user][1] <= now:
+                    del self.diclient[user]
+                    break
+            i = i + 1
+
+  def handle(self):
+        """Handle method of the server."""
+        if self.dic_clients == {}:
+            self.json2register()
+        self.wfile.write(b"Hemos recibido tu peticion ")
         for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
+            informacion = line.decode('utf-8').split(" ")
+         if (informacion[0] == "REGISTER"):
+                user = informacion[1].split(':')[1]
+                self.dicc_registers[user] = [IP]
+                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            elif (informacion[0] == "EXPIRES:"):
+                TIME = informacion[1].split(':')[-1]
+                then = time.strftime(
+                        '%Y-%m-%d %H:%M:%S', time.gmtime(
+                                time.time() + float((expires))))
+                self.dic_clients[sip_address]["expires"] = then
+                if expires == '0':
+                    del self.dic_clients[sip_address]
+                else:
+                    self.expires()
+        print("Nuevo usuario registrado")
+        self.register2json()
 
 if __name__ == "__main__":
-    # Listens at localhost ('') port 6001 
-    # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', 6001), EchoHandler) 
-
-    print("Lanzando servidor UDP de eco...")
+    # Listens at localhost ('') port 6001
+    # and calls the SIPRegisterHandler class to manage the request
     try:
-        serv.serve_forever()
-    except KeyboardInterrupt:
-        print("Finalizado servidor")
+        serv = socketserver.UDPServer(('',int(sys.argv[1])), SIPRegisterHandler)
+        print("Lanzando servidor UDP de eco...")
+        try:
+            serv.serve_forever()
+        except KeyboardInterrupt:
+            print("Finalizado servidor")
+    except (IndexError, ValueError, PermissionError):
+        print("Usage: phython3 server.py puerto")
